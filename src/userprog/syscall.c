2cbb9b3ef9ede50b90906781e6918a70714a2923
Rebecca Yeung <ryeung@locust.rlogin>
2024-03-21 17:49:42 -0400

    typo

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -8,7 +8,7 @@ static void syscall_handler(struct intr_frame *);
 void lock_file()
 {
   // ASSERT(!lock_held_by_current_thread(&file_lock));
-  if (!ock_held_by_current_thread(&file_lock))
+  if (!lock_held_by_current_thread(&file_lock))
   {
     lock_acquire(&file_lock);
   }
